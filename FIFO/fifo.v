d79a27908f49452aadb0640266dee638d3ae1762
Luis Diego Pacheco <luis.dipa@hotmail.com>
2020-10-28 13:34:21 -0600

    Ya funciona el modelo del fifo conductual y estructural. Revisar tiempos

--- FIFO/fifo.v
+++ FIFO/fifo.v
@@ -14,13 +14,12 @@ module fifo #(parameter BITNUMBER = 8,
   output reg Fifo_wr_error,
   output reg Fifo_error,
   output reg almost_full, 
-  //output reg [2:0] rd_ptr,
   output reg almost_empty
   );
 
 reg to_empty, q_wt, q_rd;
 reg [(LENGTH/2)-2:0] rd_ptr, wr_ptr,  q_w, q_r;
-reg [(LENGTH/2)-1:0] elementos, crecimiento;
+reg [(LENGTH/2)-1:0] elementos, crecimiento, contador;
 wire [BITNUMBER-1:0] q1;
 reg [BITNUMBER-1:0] q0;
 
@@ -29,13 +28,16 @@ always @(posedge clk) begin
         {wr_ptr, rd_ptr, Fifo_rd_error, Fifo_wr_error} <= 0;
         elementos <= 0 ;
         crecimiento <= 0;
-        to_empty <= 1;
         q0 <= 0;
         Fifo_Data_out <= 0;
         q_w <= 0;
         q_wt <= 0;
-        Fifo_empty <= 0;
+        Fifo_empty <= 1;
         Fifo_full <= 0;
+        contador <= 0;
+        Fifo_error <= 0;
+        almost_empty <= 0;
+        almost_full <= 0;
     end
     else begin
         q_w <= wr_ptr;
@@ -45,17 +47,10 @@ always @(posedge clk) begin
         elementos <= (wr_ptr - rd_ptr + LENGTH) % LENGTH ;
         crecimiento <= elementos;
         Fifo_Data_out <= q1;
-        if (elementos != 0 && crecimiento != 0)begin
-           if (elementos > crecimiento) // se esta llenando
-            to_empty <= 0;
-        if (elementos < crecimiento)//se esta vaciando
-            to_empty <= 1;
-        if (elementos == crecimiento)
-            to_empty <= to_empty; 
-        end
 
         if (Fifo_wr && !Fifo_full) begin
             wr_ptr <= wr_ptr + 1;
+            contador = contador +1;
             Fifo_wr_error <= 0;
             q0 <= Fifo_Data_in; 
         end
@@ -69,6 +64,7 @@ always @(posedge clk) begin
         end
         if(Fifo_rd && !Fifo_empty) begin
             rd_ptr <= rd_ptr + 1;
+            contador = contador -1;
             Fifo_rd_error <= 0; 
             Fifo_Data_out <= q1;
             //invalid_read <= 0;
@@ -79,42 +75,23 @@ always @(posedge clk) begin
             Fifo_rd_error <= 0; // si comento esto la señal se mantiene hasta el proximo read sin error
         /*else 
             invalid_read <= 1;*/
-    end
-end
-
-
-always @(*) begin
-    elementos = (wr_ptr - rd_ptr + LENGTH) % LENGTH ;
-    Fifo_empty = 0;
-    Fifo_full = 0;
-    {almost_empty, almost_full} = 0;
-    Fifo_error = 0;
-    if (reset) begin
-        elementos = 0;
-        Fifo_empty = 0;
-        Fifo_full = 0;
-        {almost_empty, almost_full} = 0;
-        to_empty = 0;
-    end
-    else begin
-        if(elementos == 0) begin
-            if (to_empty) begin
-                Fifo_empty = 1;
-                to_empty = 1;
-                //wr_ptr = 0;
-                //rd_ptr = 0;
-            end
+        if (contador == 8)
+            Fifo_full <= 1;
         else 
-            Fifo_full = 1;
-            //to_empty = 0;
-    end
-    if(elementos == 1) begin
-        almost_empty = 1;
-    end
-    if(elementos == 7) begin
-        almost_full = 1;
-    end
-    Fifo_error = Fifo_rd_error | Fifo_wr_error; 
+            Fifo_full <= 0;
+        if (contador == 0)
+            Fifo_empty <= 1;
+        else
+            Fifo_empty <= 0;
+        if (contador == 7)
+            almost_full <= 1;
+        else 
+            almost_full <= 0;
+        if (contador == 1)
+            almost_empty <= 1;
+        else
+            almost_empty <= 0;
+        Fifo_error <= Fifo_rd_error | Fifo_wr_error; 
     end
 end
 
